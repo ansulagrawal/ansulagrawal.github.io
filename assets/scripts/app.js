9560b922e7761f2b8f0b2e7737041a085d71c2a5
Ansul Agrawal <ansulagrawal9@gmail.com>
2021-07-30 17:42:31 +0530

    main commit -1

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,8 @@
-const Portfolio = function() {
+const Portfolio = function () {
 	function makeWords() {
 		var words = [
 			{
-				text: "rapping",
+				text: "es6+",
 				weight: 12.3
 			}, {
 				text: "css3",
@@ -11,37 +11,55 @@ const Portfolio = function() {
 				text: "javascript",
 				weight: 14
 			}, {
-				text: "jquery",
-				weight: 3
-			}, {
-				text: "programming",
+				text: "mongoDB",
 				weight: 7
 			}, {
-				text: "python",
-				weight: 10
-			}, {
-				text: "java",
-				weight: 9
-			}, {
-				text: "qazi",
+				text: "react",
 				weight: 15
 			}, {
-				text: "newText",
+				text: "nodeJS",
+				weight: 9
+			}, {
+				text: "html5",
+				weight: 10
+			}, {
+				text: "full-stack",
 				weight: 7
+			}, {
+				text: "sql",
+				weight: 9
+			}, {
+				text: "api",
+				weight: 13
+			}, {
+				text: "ui/ux design",
+				weight: 8
+			}, {
+				text: "product planning",
+				weight: 10
+			}, {
+				text: "int'l money transfer",
+				weight: 4
+			}, {
+				text: "scrum",
+				weight: 6
+			}, {
+				text: "end-to-end product oversight",
+				weight: 5
 			}
 		];
 		return words;
 	}
 
 	function makeWordCloud(words) {
-		$('.teaching-domains').jQCloud(words, {delay: 120});
+		$('.teaching-domains').jQCloud(words, { delay: 120 });
 	}
 
 	function displayWordCloud() {
 		var count = 1;
-		$(window).on('scroll', function() {
+		$(window).on('scroll', function () {
 			var y_scroll_pos = window.pageYOffset;
-			var scroll_pos_test = 2700; // set to whatever you want it to be
+			var scroll_pos_test = 1000; // set to whatever you want it to be
 			var words = makeWords();
 			if (y_scroll_pos > scroll_pos_test && count <= 1) {
 				makeWordCloud(words);
@@ -57,24 +75,49 @@ const Portfolio = function() {
 	function typeAnimation() {
 		Typed.new("#writing-text", {
 			strings: [
-				"am a Full-Stack Web Developer.", "love everything about code.", "also teach programming to people.", "solve problems."
+				"am a 21 year Btech CSE student",
+				"loves coding,",
+				"working as Full-Stack Web Developer.",  
+				"also solve complex problems."
 			],
 			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
 			stringsElement: null,
 			// typing speed
 			typeSpeed: 1,
 			contentType: 'text',
-			callback: function() {
-				$("#writing-text").css({"color": "#fff", "background-color": "#C8412B"});
+			callback: function () {
+				setTimeout(() => { typeAnimation() }, 500)
 			},
-			preStringTyped: function() {},
-			onStringTyped: function() {}
+			preStringTyped: function () { },
+			onStringTyped: function () { }
+		});
+	}
+
+	function typeAnimation2() {
+		Typed.new("#writing-skillset", {
+			strings: [
+				"Languages I speak",
+				"Technolgies I love",
+				"Toolbox I use"
+			],
+			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+			stringsElement: null,
+			// typing speed
+			typeSpeed: 1,
+			contentType: 'text',
+			callback: function () {
+				setTimeout(() => { typeAnimation2() }, 500)
+				// $("#writing-skillset").css({ "color": "#fff", "background-color": "#C8412B" });
+			},
+			preStringTyped: function () { },
+			onStringTyped: function () { }
 		});
 	}
 
 	return {
 		displayWordCloud: displayWordCloud,
-		typeAnimation: typeAnimation
+		typeAnimation: typeAnimation,
+		typeAnimation2: typeAnimation2
 	}
 
 }();
@@ -82,3 +125,4 @@ const Portfolio = function() {
 
 Portfolio.displayWordCloud();
 Portfolio.typeAnimation();
+Portfolio.typeAnimation2()
